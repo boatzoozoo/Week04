2caf11b1d9ee7f6bce3ff5b49ab30941728c115b
boatzoozoo <sarun0803019935@gmail.com>
2018-09-14 15:11:29 +0700

    13(2)

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -8,74 +8,63 @@ namespace Lab01
         {
             Console.WriteLine("Data type : int");
             Console.WriteLine("Size :" + sizeof(int));
-            Console.WriteLine("Minimum : " + int.MinValue);
-            Console.WriteLine("Maximum : " + int.MaxValue);
-                   
+            Console.WriteLine("Minimum Value :" + int.MinValue);
+            Console.WriteLine("Maximum Value :" + int.MaxValue);
 
             Console.WriteLine("Data type : byte");
-            Console.WriteLine("Size :" + sizeof(byte));
-            Console.WriteLine("Minimum : " + byte.MinValue);
-            Console.WriteLine("Maximum : " + byte.MaxValue);
+            Console.WriteLine("Size :" + sizeof(int));
+            Console.WriteLine("Minimum Value :" + byte.MinValue);
+            Console.WriteLine("Maximum Value :" + byte.MaxValue);
 
             Console.WriteLine("Data type : char");
-            Console.WriteLine("Size :" + sizeof(char));
-            Console.WriteLine("Minimum : " + char.MinValue);
-            Console.WriteLine("Maximum : " + char.MaxValue);
-
+            Console.WriteLine("Size :" + sizeof(int));
+            Console.WriteLine("Minimum Value :" + char.MinValue);
+            Console.WriteLine("Maximum Value :" + char.MaxValue);
 
             Console.WriteLine("Data type : sbyte");
-            Console.WriteLine("Size :" + sizeof(sbyte));
-            Console.WriteLine("Minimum : " + sbyte.MinValue);
-            Console.WriteLine("Maximum : " + sbyte.MaxValue);
-
+            Console.WriteLine("Size :" + sizeof(int));
+            Console.WriteLine("Minimum Value :" + sbyte.MinValue);
+            Console.WriteLine("Maximum Value :" + sbyte.MaxValue);
 
             Console.WriteLine("Data type : short");
-            Console.WriteLine("Size :" + sizeof(short));
-            Console.WriteLine("Minimum : " + short.MinValue);
-            Console.WriteLine("Maximum : " + short.MaxValue);
-
+            Console.WriteLine("Size :" + sizeof(int));
+            Console.WriteLine("Minimum Value :" + short.MinValue);
+            Console.WriteLine("Maximum Value :" + short.MaxValue);
 
             Console.WriteLine("Data type : ushort");
-            Console.WriteLine("Size :" + sizeof(ushort));
-            Console.WriteLine("Minimum : " + ushort.MinValue);
-            Console.WriteLine("Maximum : " + ushort.MaxValue);
-
+            Console.WriteLine("Size :" + sizeof(int));
+            Console.WriteLine("Minimum Value :" + ushort.MinValue);
+            Console.WriteLine("Maximum Value :" + ushort.MaxValue);
 
             Console.WriteLine("Data type : uint");
-            Console.WriteLine("Size :" + sizeof(uint));
-            Console.WriteLine("Minimum : " + uint.MinValue);
-            Console.WriteLine("Maximum : " + uint.MaxValue);
-
+            Console.WriteLine("Size :" + sizeof(int));
+            Console.WriteLine("Minimum Value :" + uint.MinValue);
+            Console.WriteLine("Maximum Value :" + uint.MaxValue);
 
             Console.WriteLine("Data type : float");
-            Console.WriteLine("Size :" + sizeof(float));
-            Console.WriteLine("Minimum : " + float.MinValue);
-            Console.WriteLine("Maximum : " + float.MaxValue);
-
+            Console.WriteLine("Size :" + sizeof(int));
+            Console.WriteLine("Minimum Value :" + float.MinValue);
+            Console.WriteLine("Maximum Value :" + float.MaxValue);
 
             Console.WriteLine("Data type : double");
-            Console.WriteLine("Size :" + sizeof(double));
-            Console.WriteLine("Minimum : " + double.MinValue);
-            Console.WriteLine("Maximum : " + double.MaxValue);
-
+            Console.WriteLine("Size :" + sizeof(int));
+            Console.WriteLine("Minimum Value :" + double.MinValue);
+            Console.WriteLine("Maximum Value :" + double.MaxValue);
 
             Console.WriteLine("Data type : decimal");
-            Console.WriteLine("Size :" + sizeof(decimal));
-            Console.WriteLine("Minimum : " + decimal.MinValue);
-            Console.WriteLine("Maximum : " + decimal.MaxValue);
+            Console.WriteLine("Size :" + sizeof(int));
+            Console.WriteLine("Minimum Value :" + decimal.MinValue);
+            Console.WriteLine("Maximum Value :" + decimal.MaxValue);
 
+            Console.WriteLine("Data type :  long ");
+            Console.WriteLine("Size :" + sizeof(int));
+            Console.WriteLine("Minimum Value :" + long.MinValue);
+            Console.WriteLine("Maximum Value :" + long.MaxValue);
 
-            Console.WriteLine("Data type : long");
-            Console.WriteLine("Size :" + sizeof(long));
-            Console.WriteLine("Minimum : " + long.MinValue);
-            Console.WriteLine("Maximum : " + long.MaxValue);
-
-
-
-            Console.WriteLine("Data type : ulong");
-            Console.WriteLine("Size :" + sizeof(ulong));
-            Console.WriteLine("Minimum : " + ulong.MinValue);
-            Console.WriteLine("Maximum : " + ulong.MaxValue);
+            Console.WriteLine("Data type :  ulong ");
+            Console.WriteLine("Size :" + sizeof(int));
+            Console.WriteLine("Minimum Value :" + ulong.MinValue);
+            Console.WriteLine("Maximum Value :" + ulong.MaxValue);
         }
     }
 }

f9f9f21f6b5589ada96e83828e79287576220812
boatzoozoo <sarun0803019935@gmail.com>
2018-09-15 15:48:48 +0700

    20

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -1,33 +1,27 @@
 ﻿using System;
 
 namespace Lab01
+
 {
     class Program
     {
         static void Main(string[] args)
         {
+
+            for (float i = 0; i < Math.PI * 2.0F; i += 0.3F)
             {
-                const double kmToau = 0.000000006685;
-                const double kmTomile = 0.621371192;
-                const double lightSpeed = 186000d;   // miles per second
-                double lighttime, sunto, dis, sum;
-                char k;
-                do
-                {
-
-                    Console.Write("\nPlease Distance KM = ");
-                    dis = double.Parse(Console.ReadLine());
-                    sum = dis * kmToau;      // แปลงเป็น A.U
-                    Console.WriteLine("DistanceToSun = {0:F4} A.U", sum);
-                    sunto = dis * kmTomile;    // หาระยะห่างจากดาวถึงดวงอาทิตย์เป็นหน่วย Mile
-                    Console.WriteLine("DistanceMile = {0:F4} Mile", sunto);
-                    lighttime = sunto / lightSpeed;  // ระยะห่าง(ไมล์) * ความเร็วแสง (ไมล์ต่อวิ) = ระยะเวลาของแสงจากดาวถึงอาทิตย์ (ต่อวิ)
-                    Console.WriteLine("LightSpeedTime = {0:F2} Minutes", lighttime / 60);// ระยะเวลาของแสงจากดาวถึงอาทิตย์(ต่อนาที)
-
-                    k = Console.ReadKey().KeyChar;
-                }
-                while ((k == 'Y') || (k == 'y'));
+                Console.WriteLine("The sine of {0,10:F} = {1,-10:F6}" +
+                spaces(Math.Tan(i)) + "*", i, Math.Tan(i));
             }
+        }
+        private static string spaces(double val)
+        {
+            string SpaceString = new String(' ', ((int)(val * 10.0)) + 10);
+            return SpaceString;
+
         }
     }
 }
+
+
+    
